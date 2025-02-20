b8a1cd9d2686166e276397b4a70796ece25a3f18
RehasLiamsi <nsaher91@gmail.com>
2025-02-20 11:04:43 +0100

    Create Expense Model

---
+++ Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
