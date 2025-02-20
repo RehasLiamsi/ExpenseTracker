ddabe17bc50a3a871bef53966ec8aa8f034076f0
RehasLiamsi <nsaher91@gmail.com>
2025-02-20 11:05:34 +0100

    Create ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data
+{
+    public class ApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Expenses)
+                .WithOne(e => e.User)
+                .HasForeignKey(e => e.UserId);
+        }
+    }
+}
