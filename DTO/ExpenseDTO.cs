c1bd9ca9fdd359be2f4ef8983eeaf4fa9779f6af
RehasLiamsi <nsaher91@gmail.com>
2025-02-26 11:28:45 +0100

    Create Expense DTO

---
+++ DTO/ExpenseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpenseTracker.DTO
+{
+    public class ExpenseDTO
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
