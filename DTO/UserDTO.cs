6c6315a0b59712c1f69227843172d57a461c9b94
RehasLiamsi <nsaher91@gmail.com>
2025-02-26 12:41:03 +0100

    Edit property name and make Id nullable

--- DTO/UserDTO.cs
+++ DTO/UserDTO.cs
@@ -2,9 +2,9 @@
 {
     public class UserDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        public List<int>? ExpenseId { get; set; }
+        public List<int>? ExpenseIds { get; set; }
     }
 }
