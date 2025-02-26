290e966e3fff7d121fb09f9dcd6e6ab33141f3f8
RehasLiamsi <nsaher91@gmail.com>
2025-02-26 11:27:47 +0100

    Create ExpenseController

---
+++ Controllers/ExpenseController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Data;
+using ExpenseTracker.Models;
+
+
+namespace ExpenseTracker.Controllers
+{
+    [Route("api/expenses")]
+    [ApiController]
+    public class ExpenseController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Expense>>> GetExpenses()
+        {
+            return await _context.Expenses.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Expense>> GetOneExpense(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+
+            if(expense == null)
+            {
+                return NotFound();
+            }
+
+            return expense;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Expense>> CreateExpense(Expense expense)
+        {
+            var user = await _context.Users.FindAsync(expense.UserId);
+            
+            if (user == null)
+            {
+                return BadRequest(new { message = "User witht the given ID does not exist." });
+            }
+
+            expense.User = user;
+
+            _context.Expenses.Add(expense);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetOneExpense), new { id = expense.Id }, expense);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateExpense(int id, Expense updatedExpense)
+        {
+            if (id!= updatedExpense.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(updatedExpense).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Expenses.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpense(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
