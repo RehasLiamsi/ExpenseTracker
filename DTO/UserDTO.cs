6d70be4d13304e7fd28920b430d825a8b04f238d
RehasLiamsi <nsaher91@gmail.com>
2025-02-26 11:36:17 +0100

    Create UserDTO

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public List<int>? ExpenseId { get; set; }
+    }
+}
