8eccab01fe7c32ed3ed66fba1610048bafe83a3f
RehasLiamsi <nsaher91@gmail.com>
2025-03-04 11:18:05 +0100

    Create MappingProfile.cs

---
+++ MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ExpenseTracker.DTO;
+using ExpenseTracker.Models;
+
+namespace ExpenseTracker
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Expense, ExpenseDTO>();
+        }
+    }
+}
