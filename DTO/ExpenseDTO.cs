be5e98e6db5f15cd16b40d7779f63f382aca77c2
RehasLiamsi <nsaher91@gmail.com>
2025-03-04 10:41:44 +0100

    Make fields required in ExpenseDTO

--- DTO/ExpenseDTO.cs
+++ DTO/ExpenseDTO.cs
@@ -3,9 +3,9 @@
     public class ExpenseDTO
     {
         public int Id { get; set; }
-        public string Category { get; set; }
-        public string Description { get; set; }
-        public double Amount { get; set; }
+        public required string Category { get; set; }
+        public required string Description { get; set; }
+        public required double Amount { get; set; }
         public DateTime Date { get; set; }
         public int UserId { get; set; }
 
