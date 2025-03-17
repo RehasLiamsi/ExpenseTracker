1f6239ab5149649866aa6d8e28231602d53ad095
RehasLiamsi <nsaher91@gmail.com>
2025-03-17 11:13:46 +0100

    Edit mapping

--- MappingProfile.cs
+++ MappingProfile.cs
@@ -8,7 +8,18 @@ namespace ExpenseTracker
     {
         public MappingProfile()
         {
+            CreateMap<User, UserDTO>()
+            .ForMember(dest => dest.ExpenseIds,
+               opt => opt.MapFrom(src => src.Expenses != null
+                   ? src.Expenses.Select(e => e.Id).ToList()
+                   : new List<int>()));
+
+            CreateMap<UserDTO, User>()
+            .ForMember(dest => dest.Expenses, opt => opt.Ignore());
+
             CreateMap<Expense, ExpenseDTO>();
+
+            CreateMap<ExpenseDTO, Expense>();
         }
     }
 }
